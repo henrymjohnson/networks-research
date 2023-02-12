c544c8d4c5cc6b4ec7c5009112c2afc2c676b7ba
Henry Johnson <hennrymjohnson@gmail.com>
2023-02-12 16:14:36 -0700

    Added degree distribution functions

--- src/utility_functions/network_probability.py
+++ src/utility_functions/network_probability.py
@@ -1,16 +1,47 @@
 import numpy as np
+import scipy as sp
 
-def probability_of_network(number_of_nodes, number_of_edges, link_formation_probability):
+
+def probability_of_network(number_of_nodes, degree, probability):
     """
-    Returns the probability of a network being formed given the probability of an edge between a set of nodes.
+    Returns the probability of a network being formed given the probability of an edge between two nodes.
     """
     if not (isinstance(number_of_nodes, int) and number_of_nodes > 0):
         raise ValueError('number_of_nodes must be a positive integer')
-    if not isinstance(number_of_edges, int) or number_of_edges < 0:
-        raise ValueError('number_of_edges must be a positive integer')
-    if link_formation_probability < 0 or link_formation_probability > 1:
-        raise ValueError('link_formation_probability must be a value between 0 and 1')
-    if number_of_edges > number_of_nodes * (number_of_nodes - 1) / 2:
-        raise ValueError('number_of_edges must be less than the maximum possible number of edges, {}.'.format(number_of_nodes * (number_of_nodes - 1) / 2))
+    if not isinstance(degree, int) or degree < 0:
+        raise ValueError('degree must be a positive integer')
+    if probability < 0 or probability > 1:
+        raise ValueError('probability must be a value between 0 and 1')
+    if degree > number_of_nodes * (number_of_nodes - 1) / 2:
+        raise ValueError('degree must be less than the maximum possible number of edges, {}.'.format(number_of_nodes * (number_of_nodes - 1) / 2))
 
-    return np.power(link_formation_probability, number_of_edges) * np.power(1 - link_formation_probability, number_of_nodes * (number_of_nodes - 1) / 2 - number_of_edges)
+    return np.power(probability, degree) * np.power(1 - probability, number_of_nodes * (number_of_nodes - 1) / 2 - degree)
+
+
+def degree_distribution_of_random_network(number_of_nodes, degree, probability):
+    """
+    Returns the degree distribution of a random network.
+    """
+    if not (isinstance(number_of_nodes, int) and number_of_nodes > 0):
+        raise ValueError('number_of_nodes must be a positive integer')
+    if not isinstance(degree, int) or degree < 0:
+        raise ValueError('degree must be a positive integer')
+    if probability < 0 or probability > 1:
+        raise ValueError('probability must be a value between 0 and 1')
+
+    return sp.special.binom(number_of_nodes - 1, degree) * np.power(probability, degree) * np.power(1 - probability, number_of_nodes - 1 - degree)
+
+
+def degree_distribution_approximation(number_of_nodes, degree, probability):
+    """
+    For large enough networks with small link probabilities, the degree distribution of a random network can be approximated 
+    by a Poisson distribution. So the fraction of nodes with links numbering the degree can be approximated by the following
+    """
+    if not (isinstance(number_of_nodes, int) and number_of_nodes > 0):
+        raise ValueError('number_of_nodes must be a positive integer')
+    if not isinstance(degree, int) or degree < 0:
+        raise ValueError('degree must be a positive integer')
+    if probability < 0 or probability > 1:
+        raise ValueError('probability must be a value between 0 and 1')
+
+    return np.exp(-(number_of_nodes - 1) * probability) * ((number_of_nodes - 1) * probability) ** degree / sp.special.factorial(degree)
--- tests/utility_functions/test_network_probability.py
+++ tests/utility_functions/test_network_probability.py
@@ -1,5 +1,8 @@
 import pytest
-from utility_functions.network_probability import probability_of_network
+import numpy as np
+
+from utility_functions.network_probability import probability_of_network, degree_distribution_of_random_network, degree_distribution_approximation
+
 
 class TestProbabilityOfNetwork(object):
     def test_prob_fifty_two_nodes_one_edge(self):
@@ -20,13 +23,71 @@ class TestProbabilityOfNetwork(object):
     def test_negative_prob_with_two_nodes_one_edge(self):
         with pytest.raises(ValueError) as excinfo:
             probability_of_network(2, 1, -0.5)
-        assert 'link_formation_probability must be a value between 0 and 1' in str(excinfo.value)
+        assert 'probability must be a value between 0 and 1' in str(excinfo.value)
     
     def test_prob_fifty_and_two_nodes_with_negative_one_edge(self):
         with pytest.raises(ValueError) as excinfo:
             probability_of_network(2, -1, 0.5)
-        assert 'number_of_edges must be a positive integer' in str(excinfo.value)
+        assert 'degree must be a positive integer' in str(excinfo.value)
     def test_edges_more_than_maximum_possible(self):
         with pytest.raises(ValueError) as excinfo:
             probability_of_network(2, 3, 0.5)
-        assert 'number_of_edges must be less than the maximum' in str(excinfo.value)
+        assert 'degree must be less than the maximum' in str(excinfo.value)
+
+
+class TestDegreeDistributionOfRandomNetwork(object):
+    def test_prob_fifty_one_node_one_edge(self):
+        assert pytest.approx(degree_distribution_of_random_network(1, 1, 0.5)) == 0
+    def test_prob_fifty_two_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_of_random_network(2, 1, 0.5)) == 0.5
+    def test_prob_zero_two_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_of_random_network(2, 1, 0)) == 0
+    def test_prob_hundred_two_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_of_random_network(2, 1, 1)) == 1
+    def test_prob_fifty_two_nodes_two_edges(self):
+        assert pytest.approx(degree_distribution_of_random_network(2, 2, 0.5)) == 0
+    def test_prob_fifty_three_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_of_random_network(3, 1, 0.5)) == 0.5
+
+    def test_prob_fifty_zero_nodes_zero_edges(self):
+        with pytest.raises(ValueError) as excinfo:
+            degree_distribution_of_random_network(0, 0, 0.5)
+        assert 'number_of_nodes must be a positive integer' in str(excinfo.value)
+    def test_negative_prob_with_two_nodes_one_edge(self):
+        with pytest.raises(ValueError) as excinfo:
+            degree_distribution_of_random_network(2, 1, -0.5)
+        assert 'probability must be a value between 0 and 1' in str(excinfo.value)
+    
+    def test_prob_fifty_and_two_nodes_with_negative_one_edge(self):
+        with pytest.raises(ValueError) as excinfo:
+            degree_distribution_of_random_network(2, -1, 0.5)
+        assert 'degree must be a positive integer' in str(excinfo.value)
+
+
+class TestDegreeDistributionApproximation(object):
+    def test_prob_fifty_one_node_one_edge(self):
+        assert pytest.approx(degree_distribution_approximation(1, 1, 0.5)) == 0
+    def test_prob_fifty_two_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_approximation(2, 1, 0.5)) == 0.3032653
+    def test_prob_zero_two_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_approximation(2, 1, 0)) == 0
+    def test_prob_hundred_two_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_approximation(2, 1, 1)) == np.exp(-1)
+    def test_prob_fifty_two_nodes_two_edges(self):
+        assert pytest.approx(degree_distribution_approximation(2, 2, 0.5)) == 0.0758163
+    def test_prob_fifty_three_nodes_one_edge(self):
+        assert pytest.approx(degree_distribution_approximation(3, 1, 0.5)) == 0.3678794
+    def test_prob_twenty_five_hundred_nodes_five_edges(self):
+        assert pytest.approx(degree_distribution_approximation(500, 5, 0.25)) == 0.0000000
+    def test_prob_fifty_hundred_nodes_five_edges(self):
+        assert pytest.approx(degree_distribution_approximation(10, 2, 0.75)) == 0.0266741
+
+    def test_prob_fifty_zero_nodes_zero_edges(self):
+        with pytest.raises(ValueError) as excinfo:
+            degree_distribution_approximation(0, 0, 0.5)
+        assert 'number_of_nodes must be a positive integer' in str(excinfo.value)
+    def test_negative_prob_with_two_nodes_one_edge(self):
+        with pytest.raises(ValueError) as excinfo:
+            degree_distribution_approximation(2, 1, -0.5)
+        assert 'probability must be a value between 0 and 1' in str(excinfo.value)
+    
